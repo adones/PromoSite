cba30e6f915913985b603ecceb1b1ac29dcefdca
adones <adones.fuentes@gmail.com>
2013-12-12 12:18:28 +0400

    Test upload

---
+++ homepage.php
@@ -0,0 +1,376 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Promohive - Homepage</title>
+<link rel="stylesheet" type="text/css" href="css/base.css">
+<script type="text/javascript" src="js/jquery/jquery-min-1.5.0.js"></script>
+
+<script src="js/jquery/jquery-min-1.5.0.js" type="text/javascript" charset="utf-8"></script>  
+
+<script src="js/jquery/loopedslider/loopedslider.js" type="text/javascript" charset="utf-8"></script>  
+<script src="js/typeface-0.15.js"></script>
+<script src="js/helvetica-neue.js"></script>
+</head>
+
+<body>
+<div class="mainContainer">
+	<div class="header">
+    	<div class="logo">
+        	<a href="#"><img src="images/logo.png" alt="promohive-logo" /></a>
+        </div>
+        <div class="topBanner">
+        	<img src="images/top-banner.png" alt="banner-top" />
+        </div>
+    </div>
+    <div class="contentContainer">
+    	<div class="homeBanner">
+        	<div class="leftBnr">
+            	<img src="images/mid-banner-left.png" />
+            </div>
+            <div class="centerBnr">
+            	<div class="slides">
+                	<div><img src="images/mid-banner-center.png" /></div>
+                	<div><img src="images/mid-banner-center.png" /></div>
+                	<div><img src="images/mid-banner-center.png" /></div>
+                </div>
+            </div>
+            <div class="rightBnr">
+            	<div class="rightBnrTop">
+                	<img src="images/mid-banner-right-top.png" />
+                </div>
+            	<div class="rightBnrBtm">
+                	<img src="images/mid-banner-right-bottom.png" />
+                </div>
+            </div>
+            <div class="clrFloat"></div>
+        </div>
+        
+    	<div class="menu">
+        	<!-- <img src="images/menu-items.png" /> -->
+            <ul class="mainCategory">
+            	<li><a href="#" class=" typeface-js">COUPONS</a></li>
+            	<li><a href="#" class=" typeface-js">COMPETITIONS</a></li>
+            	<li><a href="#" class=" typeface-js">FEATURES</a></li>
+            	<li><a href="#" class=" typeface-js">NEWSLETTER</a></li>
+            	<li><a href="#" class=" typeface-js">TICKETS</a></li>
+            	<li><a href="#" class=" typeface-js">AWARDS & POLLING</a></li>
+            	<li><a href="#" class=" typeface-js">EVENTS</a></li>
+        	</ul>
+            
+            <ul class="subCategory">
+            	<li class="first"><a href="#" class="arCity typeface-js">AROUND THE CITY</a></li>
+            	<li><a href="#" class="arts typeface-js">ARTS</a></li>
+            	<li><a href="#" class="barClub typeface-js">BARS & CLUBS</a></li>
+            	<li><a href="#" class="bodyMind typeface-js">BODY & MIND</a></li>
+            	<li><a href="#" class="community typeface-js">COMMUNITY</a></li>
+            	<li><a href="#" class="film typeface-js">FILM</a></li>
+            	<li><a href="#" class="hotel typeface-js">HOTEL</a></li>
+            	<li><a href="#" class="kids typeface-js">KIDS</a></li>
+            	<li><a href="#" class="music typeface-js">NIGHTLIFE & MUSIC</a></li>
+            	<li><a href="#" class="restaurant typeface-js">RESTAURANT</a></li>
+            	<li><a href="#" class="shopping typeface-js">SHOPPING</a></li>
+            	<li><a href="#" class="sports typeface-js">SPORTS & OUTDOOR</a></li>
+            	<li><a href="#" class="travel typeface-js">TRAVEL</a></li>
+            </ul>
+            <div class="clrFloat"></div>
+            <!-- <ul>
+            	<li><a href="#">asdf</a></li>
+                <li><a href="#">asdf</a></li>
+                <li><a href="#">asdf</a></li>
+            </ul> -->
+        </div>
+    	<div class="contentDetails">
+        	<div class="homeLeft">
+
+                 <!-- for restaurant ads -->
+            	<div class="buzz">
+                	<div class="buzzHead typeface-js" >
+                    	<span>Buzz of the Week</span><a href="#"><img src="images/view-all-btn.png" /></a>
+                    </div>
+                    <div class="buzzContent">
+                    	<ul>
+                        	<li class="bList1">
+                            	<img src="images/buzz-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                            <li class="bList2">
+                            	<img src="images/buzz-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                            <li class="bList3">
+                            	<img src="images/buzz-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                        </ul>
+                        <div class="clrFloat"></div>
+                    </div>
+                </div>
+                
+                <!-- latest photo -->
+                <div class="lphoto typeface-js">
+                	<div class="lphotoHead">
+                    	<span>Latest Photos</span><a href="#"><img src="images/view-all-btn.png" /></a>
+                    </div>
+                    <div class="lphotoContent">
+                    	<ul>
+                        	<li class="bList1">
+                            	<img src="images/latest-photo-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                            <li class="bList2">
+                            	<img src="images/latest-photo-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                            <li class="bList3">
+                            	<img src="images/latest-photo-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                        </ul>
+                        <div class="clrFloat"></div>
+                    </div>
+                </div>
+                
+                <!-- for restaurant ads -->
+                <div class="resto">
+                	<div class="restoHead typeface-js">
+                    	<span>Restaurant Promos</span><a href="#"><img src="images/view-all-btn.png" /></a>
+                    </div>
+                    <div class="restoContent">
+                    	<ul>
+                        	<li class="bList1">
+                            	<img src="images/restaurant-promo-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                            <li class="bList2">
+                            	<img src="images/restaurant-promo-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                            <li class="bList3">
+                            	<img src="images/restaurant-promo-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                        </ul>
+                        <div class="clrFloat"></div>
+                    </div>
+                </div>
+
+                <!-- for kids ads -->
+                <div class="kids">
+                	<div class="kidsHead typeface-js">
+                    	<span>Kids Promos</span><a href="#"><img src="images/view-all-btn.png" /></a>
+                    </div>
+                    <div class="kidsContent">
+                    	<ul>
+                        	<li class="bList1">
+                            	<img src="images/kids-promo-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                            <li class="bList2">
+                            	<img src="images/kids-promo-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                            <li class="bList3">
+                            	<img src="images/kids-promo-image.png" />
+                            	<h3>Special Offer All BMW Series</h3>
+                                <p>Donec cons ectetur libero nec tellus posuere malesuada. </p>
+                                <a href="#">View More</a>
+                            </li>
+                        </ul>
+                        <div class="clrFloat"></div>
+                    </div>
+                </div>
+            </div>
+        	<div class="homeCenter">
+                <div class="print">
+                	<img src="images/print-image.png" />
+                </div>
+                <div class="promoHomeList">
+                	<div class="promoHomeListHead typeface-js">
+                    	Promos in Dubai
+                    </div>
+                	<ul>
+                    	<li><a href="#"> Best Dubai Discountz</a></li>
+                    	<li><a href="#"> Best Dubai Discountz</a></li>
+                    	<li><a href="#"> Best Dubai Discountz</a></li>
+                    	<li><a href="#"> Best Dubai Discountz</a></li>
+                    	<li><a href="#"> Best Dubai Discountz</a></li>
+                    </ul>
+                </div>
+                <div class="whatBuzz">
+                	<div class="whatBuzzHead typeface-js">
+                    	<span>What'z the buzz</span>
+                    </div>
+                	<ul>
+                    	<li>
+                        	<h3>Middle East tourist cruise the gulf</h3>
+                        	<p>European tour company sees boost in Middle East cruises...</p>
+                        </li>
+                    	<li>
+                        	<h3>Middle East tourist cruise the gulf</h3>
+                        	<p>European tour company sees boost in Middle East cruises...</p>
+                        </li>
+                    	<li>
+                        	<h3>Middle East tourist cruise the gulf</h3>
+                        	<p>European tour company sees boost in Middle East cruises...</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="cntntCenterBnr">
+                	<img src="images/mid-content-banner-ad.png" />
+                </div>
+                <div class="giveBuzz">
+                	<div class="giveBuzzHead typeface-js">
+                    	<span>Give a buzz</span>
+                    </div>
+                    <p>Did you move to Dubai to save money to take home?</p>
+                	<ul>
+                    	<li>
+                    		<input type="radio" value="0" />Yes    	
+                        </li>
+                    	<li>
+                        	<input type="radio" value="1" />No
+                        </li>
+                    	<li>
+                        	<input type="radio" value="2" />I didn't plan to - but I have anyway
+                        </li>
+                    </ul>
+                    <div class="voteBtn"><img src="images/vote.png" /></div>
+                </div>
+            </div>
+        	<div class="homeRight">
+            	<div class="barGraph">
+                	<img src="images/bar-graph.png" />
+                </div>
+                <div class="cntntRightBnr">
+                    <img src="images/right-content-banner.png" />
+                </div>
+                <div class="ourFav">
+                	<div class="ourFavHead typeface-js">
+                    	<span>Our Favourite Promos</span>
+                    </div>
+					<ul>
+                    	<li class="list1">
+                        	<img src="images/our-favourite-image.png" />
+                        	<h3>Cheapest Dubai Party Bars</h3>
+                        	<p>Thanks for making 2010 such a brilliant clubbing year everybody.</p>
+                        </li>
+                    	<li class="list2">
+                        	<img src="images/our-favourite-image.png" />
+                        	<h3>Cheapest Dubai Party Bars</h3>
+                        	<p>Thanks for making 2010 such a brilliant clubbing year everybody.</p>
+                        </li>
+                    	<li class="list3">
+                        	<img src="images/our-favourite-image.png" />
+                        	<h3>Cheapest Dubai Party Bars</h3>
+                        	<p>Thanks for making 2010 such a brilliant clubbing year everybody.</p>
+                        </li>
+                    	<li class="list4">
+                        	<img src="images/our-favourite-image.png" />
+                        	<h3>Cheapest Dubai Party Bars</h3>
+                        	<p>Thanks for making 2010 such a brilliant clubbing year everybody.</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="fb">
+                	<div class="fbHead">
+                    	<div class="imgDisp"><img src="images/promohive-on-fb.png" /></div>
+                        <div class="fbLike"><img src="images/fb-like-btn.png" /></div>
+                        <p>8,752 people like</p>
+                    </div>
+                	<ul>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    	<li>
+                        	<a href="#"><img src="images/facebook-profile-pic.png" /></a>
+                        	<a href="#">Leslie</a>
+                        </li>
+                    </ul>
+                    <div class="clrFloat"></div>
+                    <div class="fbFoot">
+                    	<img src="images/fb-icon.png" /><span>Facebook social plugin</span>
+                    	<div class="clrFloat"></div>
+                    </div>
+                    <div class="clrFloat"></div>
+                </div>
+            </div>
+                <div class="clrFloat"></div>
+
+        </div>
+        <div class="clrFloat"></div>
+    <div class="addBottom">
+    	<a href="#" class="gAds"><img src="images/bottom-banner.png" /></a><a href="#" class="mobLink"><img src="images/mobile-btn.png" /></a>
+    </div>
+    <div class="footer">
+    	<img src="images/footer-menu.png" /><img src="images/dummu-footer_11.png" />
+    </div>
+    </div>
+</div>	
+
+<script type="text/javascript" charset="utf-8">
+	$(function(){
+		$('.centerBnr').loopedSlider({
+			autoStart: 4000
+		});
+		
+	});
+</script>
+
+</body>
+</html>
